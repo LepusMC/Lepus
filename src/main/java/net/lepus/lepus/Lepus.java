eead966c828b2b7f67440495433afe4a961bcbcd
kgpdx <krysppy@gmail.com>
2020-10-26 02:28:08 -0500

    Initial commit

---
+++ src/main/java/net/lepus/lepus/Lepus.java
@@ -0,0 +1,17 @@
+package net.lepus.lepus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lepus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
